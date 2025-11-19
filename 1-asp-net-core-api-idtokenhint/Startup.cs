9e72e1e92a1de93dd7931ebf9a536a771d7c6844
Fidaa Mahboob <fidaamahboob@Mac.lan>
2025-11-19 12:18:55 +0000

    Adding the app insights code aparatus

--- 1-asp-net-core-api-idtokenhint/Startup.cs
+++ 1-asp-net-core-api-idtokenhint/Startup.cs
@@ -19,9 +19,14 @@ namespace AspNetCoreVerifiableCredentials
 
         public IConfiguration Configuration { get; }
 
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+            services.AddControllersWithViews();
 
             services.Configure<KestrelServerOptions>(options =>
             {
